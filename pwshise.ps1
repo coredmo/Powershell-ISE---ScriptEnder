52eb9df7129eddf97f4fc115975681672ec058f9
canner <credsecond@gmail.com>
2023-12-28 08:50:08 -0800

    -funny business

--- pwshise.ps1
+++ pwshise.ps1
@@ -94,21 +94,17 @@ help: List this menu
 new: Create a new Powershell script file
 open: Select an existing .ps1 file as active
 edit: It's prophesized to at least contain something
-search | ad | s: Search your PC's active directory computer descriptions and query for MAC addresses
-wake   | wol: Send a magic packet to a MAC Address, UDP via port 7
-exprs  |  rs: Restart and open Windows Explorer
 booyeah:
 
 You can add a file path after a command or create a new script in the current directory
 Example: open C:\users\$username\my creation.ps1
-Example: new here   |   nh
 
 Often times Y = "e" and N = "q"
 
 (Debug) fit: fill dir
         e: check dir
 
-- Connor's Scripted ISE & Toolkit -
+- Connor's Scripted ISE -
 https://github.com/coredmo/Powershell-ISE---ScriptEnder`n
 "@
             Write-Host "Press any key to return..."
@@ -300,96 +296,6 @@ Write-Host "Q: Quit without saving"
             }
         }
 
-        # Search the local active directory's computer descriptions
-        {$_ -in "ad", "search", "s"} {
-            $adMode = $true
-            $pingConfig = $false
-            $adLoop = $true
-            while ($adLoop) {
-                Write-Host "Do you want to enable host pinging? Y | Yes - N | No"
-                $adInput = $Host.UI.RawUI.ReadKey("IncludeKeyDown,NoEcho").Character
-                if ($adInput -ieq "y" -or $adInput -ieq "e") { $pingConfig = $true; $adLoop = $false; [System.Console]::Clear(); "Pinging each selected host" }
-                if ($adInput -ieq "n" -or $adInput -ieq "q") { $pingConfig = $false; $adLoop = $false; [System.Console]::Clear(); "Skipping ping function"}
-            }
-            while ($adMode -eq $true) {
-                do {
-                    $input = Read-Host "Enter the first or last name of the associate you want to search for`n You can press 'c' while it's querying to abort the process`n>"
-                    if (-not $input) {
-                        [System.Console]::Clear();
-                        $host.UI.RawUI.ForegroundColor = "Red"
-                        Write-Host "Error: Input cannot be blank. Please enter a valid name."
-                        $host.UI.RawUI.ForegroundColor = $orig_fg_color
-                    }
-                } while (-not $input)
-
-                $host.UI.RawUI.ForegroundColor = "Yellow"
-                Write-Host "Showing results for $input`n"
-                $host.UI.RawUI.ForegroundColor = $orig_fg_color
-
-                $result = Get-ADComputer -Filter "Description -like '*$input*'" -Properties Description
-
-                if ($result) {
-                    foreach ($computer in $result) { $cancelResult = $false
-                        if ([System.Console]::KeyAvailable -and [System.Console]::ReadKey().Key -eq "C") {
-                            Write-Host " button pressed:`nAborting query..."
-                            Start-Sleep -Milliseconds 60
-                            $cancelResult = $true
-                        }
-                        if ($cancelResult -eq $false) {                            
-                            Write-Host "$($computer.Name), $($computer.Description)"
-                            $nsResult = nslookup $($computer.Name)
-                            $nsRegex = "(?:\d{1,3}\.){3}\d{1,3}(?!.*(?:\d{1,3}\.){3}\d{1,3})"
-                            $nsMatches = [regex]::Matches($nsResult, $nsRegex)
-                            if ($pingConfig -eq $true) { 
-                                $pingResult = ping -n 1 $nsMatches
-                                if (-not $?) { $host.UI.RawUI.ForegroundColor = "Red"; "$pingResult"; $host.UI.RawUI.ForegroundColor = $orig_fg_color }
-                                else { $host.UI.RawUI.ForegroundColor = "Green"; "The host '$nsMatches' was pinged successfully"; $host.UI.RawUI.ForegroundColor = $orig_fg_color }
-                            }
-                            $macResult = arp -a | findstr "$nsMatches"
-                            if ($macResult -eq $null -or $macResult -eq '') {
-                                if ($pingResult -like "*Request timed out.*" -or $pingResult -like "*could not find host*") { $diffLan = $false } else { $diffLan = $true }
-                                if ($diffLan -eq $true) {
-                                $host.UI.RawUI.ForegroundColor = "Yellow"
-                                Write-Host "This host may be in a different LAN"
-                                $host.UI.RawUI.ForegroundColor = $orig_fg_color
-                                $diffLan = $false
-                                }
-                            }
-                            $host.UI.RawUI.ForegroundColor = "Yellow"; Write-Host "$macResult`n"; $host.UI.RawUI.ForegroundColor = $orig_fg_color
-                            "----------------------------"
-                        } else { break }
-                    }
-                } else {
-                    Write-Host "No results found for $input`n"
-                }
-                Write-Host "`nPress any key to refresh...`nPress c to return to the command line..."
-                $adOption = [System.Console]::ReadKey().Key; [System.Console]::Clear();
-                if ($adOption -ieq "c") { $adMode = $false; $correct = $true}
-            }
-        }
-
-        # Send a magic packet to a MAC Address, UDP via port 7
-        {$_ -in "wake","wol","w"} {
-            $wolMode = $true
-            while ($wolMode -eq $true) {
-	            $mac = Read-Host "Input a MAC Address or leave it blank to return"
-                if ($mac -eq $null -or $mac -eq '') { $wolMode = $false; $correct = $true; Clear-Host } 
-                else {
-	                $macByteArray = $mac -split "[:-]" | ForEach-Object { [Byte] "0x$_"}
-	                [Byte[]] $magicPacket = (,0xFF * 6) + ($macByteArray  * 16)
-	                $udpClient = New-Object System.Net.Sockets.UdpClient
-	                $udpClient.Connect(([System.Net.IPAddress]::Broadcast),7)
-	                $udpClient.Send($MagicPacket,$MagicPacket.Length)
-	                $udpClient.Close()
-	                Start-Sleep -Seconds 3
-                    Write-Host "$mac --- $macByteArray"
-                }
-            }
-        }
-
-        # Restart and open Windows Explorer
-        {$_ -in "exprs","rs"} { Stop-Process -Name explorer -Force; Start-Process explorer; $correct = $true }
-
         # Check what the directory is (Debug)
         "e" { "$dirInput <Active- dirs -Potential> $dirPart"; "$fileExtension"; [System.Console]::ReadKey().Key; [System.Console]::Clear(); }
 
@@ -429,16 +335,12 @@ Write-Host "Q: Quit without saving"
             Start-Sleep -Milliseconds 1000
             $correct = $true
             while ($true) { 
-                Write-Host "Press A-debug or B-fibba"
+                Write-Host "Press any key to enter the debug section..."
                 $choose = [System.Console]::ReadKey().Key
                 [System.Console]::Clear()
-                if ($choose -ieq "a") {
-                    $option = "debug"
-                    break
-                } elseif ($choose -ieq "b") {
-                    $option = "fibba" 
-                    break
-            } Clear-Host }
+                $option = "debug"
+                break
+            }
         }
     }
 
@@ -451,29 +353,6 @@ Write-Host "Q: Quit without saving"
     $host.UI.RawUI.ForegroundColor = "Red"
     Write-Host "Please input an actual command or follow the latter half of these here instructions"
     $host.UI.RawUI.ForegroundColor = $orig_fg_color
-}
-
-while ($option -eq "fibba") {
-    Write-Host "`nDebug & test section 1"
-    Write-Host "`nPress any key to start the Fibonacci Mode..."
-    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-    Clear-Host
-    # Fib(b)onacci Mode
-    $fib = 1
-    $bon = 0
-    while ($true) {
-        "`nFibonacci Mode: Active`nPress C to cancel`n"
-        $acci = $fib + $bon
-        $fib = $bon
-        $bon = $acci
-        $host.UI.RawUI.ForegroundColor = "Green"
-        "     $acci"
-        $host.UI.RawUI.ForegroundColor = $orig_fg_color
-        Start-Sleep -Milliseconds 500
-        Clear-Host
-        if ([System.Console]::KeyAvailable -and [System.Console]::ReadKey().Key -eq "C") { [System.Console]::Clear(); break }
-    }
-    Start-Sleep -Milliseconds 500
 }
 
 while ($option -eq "debug") {
