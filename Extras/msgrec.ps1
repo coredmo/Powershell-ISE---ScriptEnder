6c21976dcc9d5f1b61eb0294d8d978d0572ccdef
canner <credsecond@gmail.com>
2024-03-26 17:06:42 -0700

    Added inforequest option in Invoke-Explorer to get more info from a host PC
    Uses two external scripts to open a TCP connection on port 7734. WIP (Not working as intended)

---
+++ Extras/msgrec.ps1
@@ -0,0 +1,18 @@
+﻿Invoke-Command -ScriptBlock {
+    param(
+        [int]$ListeningPort = 7734
+    )
+    
+    $listener = New-Object System.Net.Sockets.TcpListener '0.0.0.0', $ListeningPort
+    $listener.Start()
+    
+    Write-Host "Listening on port $ListeningPort..."
+    
+    $client = $listener.AcceptTcpClient()
+    $stream = $client.GetStream()
+    $reader = New-Object System.IO.StreamReader($stream)
+    $message = $reader.ReadLine()
+    
+    $listener.Stop()
+    Read-Host "$message"
+}
